70bb5c751a5cae8747faa5e25e0a9f2a8d8199a1
wali999 <waliullah.neo@gmail.com>
2025-11-10 13:35:10 +0600

    Navbar and Footer added

---
+++ src/Layout/RootLayout.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from '../components/Navbar/Navbar';
+import { Outlet } from 'react-router';
+import Footer from '../components/Footer/Footer';
+
+const RootLayout = () => {
+    return (
+        <div className='max-w-7xl mx-auto'>
+            <Navbar></Navbar>
+            <Outlet></Outlet>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default RootLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import RootLayout from './Layout/RootLayout.jsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router}></RouterProvider>
   </StrictMode>,
 )
